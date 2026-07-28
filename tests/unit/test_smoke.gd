9bc79ab5dce153d9461e05d25be85dd7eab5eef6
OIT Student <oit-student@ad.unlv.edu>
2026-07-28 11:11:05 -0700

    chore: replace Godot scaffold with web toolchain

--- tests/unit/test_smoke.gd
+++
@@ -1,8 +0,0 @@
-class_name TestSmoke
-extends RefCounted
-
-func run() -> Array[String]:
-	var failures: Array[String] = []
-	if 2 + 2 != 4:
-		failures.append("test_arithmetic: expected 4")
-	return failures
